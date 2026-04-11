1f0f485ec146f263bede83108f510848a08d98fc
Alexey Gribchenko <gribchenko.2003@mail.ru>
2026-04-11 20:02:07 +0300

    refactor: changed writer; feat: added table, color for status

--- go.mod
+++ go.mod
@@ -12,9 +12,21 @@ require (
 
 require (
 	github.com/BurntSushi/toml v1.2.1 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/clipperhouse/displaywidth v0.10.0 // indirect
+	github.com/clipperhouse/uax29/v2 v2.6.0 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
+	github.com/fatih/color v1.18.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.19 // indirect
+	github.com/olekukonko/cat v0.0.0-20250911104152-50322a0618f6 // indirect
+	github.com/olekukonko/errors v1.2.0 // indirect
+	github.com/olekukonko/ll v0.1.6 // indirect
+	github.com/olekukonko/tablewriter v1.1.4 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
+	golang.org/x/sys v0.38.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
 )
--- internal/interface/cli/list.go
+++ internal/interface/cli/list.go
@@ -3,13 +3,21 @@ package cli
 import (
 	"errors"
 	"fmt"
+	"os"
 
 	"github.com/AlexeyGribchenko/task-tracker-cli/internal/domain"
 	"github.com/AlexeyGribchenko/task-tracker-cli/internal/utils"
+	"github.com/fatih/color"
+	"github.com/olekukonko/tablewriter"
+	"github.com/olekukonko/tablewriter/renderer"
 )
 
 var (
 	ErrGetTasksFailed = errors.New("Failed to get list of tasks")
+)
+
+const (
+	maxColumnWidth = 50
 )
 
 func (a *App) List(args []string) error {
@@ -24,13 +32,21 @@ func (a *App) List(args []string) error {
 		return nil
 	}
 
-	// ID, Name, Description, Creation time, Update time, Status
-	pattern := "%v\t%s\t%s\t%s\t%s\t%s"
+	colorConfig := renderer.ColorizedConfig{
+		Header: renderer.Tint{
+			FG: renderer.Colors{color.Bold},
+		},
+		Column: renderer.Tint{
+			FG: renderer.Colors{color.FgWhite},
+		},
+	}
 
-	str := fmt.Sprintf(pattern, "ID", "Task name", "Description", "Created", "Updated", "Status")
-
-	defer a.writer.Flush()
-	a.writer.Print(str)
+	table := tablewriter.NewTable(os.Stdout,
+		tablewriter.WithRenderer(renderer.NewColorized(colorConfig)),
+		tablewriter.WithRowMaxWidth(maxColumnWidth),
+	)
+	// TODO: make it configurable
+	table.Header("ID", "Task name", "Description", "Created", "Updated", "Status")
 
 	for _, task := range tasks {
 		status := task.Status
@@ -48,15 +64,17 @@ func (a *App) List(args []string) error {
 			statusStr = a.colorer.Red(statusStr)
 		}
 
-		a.writer.Print(fmt.Sprintf(pattern,
-			task.ID,
+		row := []string{
+			fmt.Sprintf("%d", task.ID),
 			task.Name,
 			utils.ValueFromPointer(task.Description),
 			task.CreatedAt.Format("15:04 02.01"),
 			task.UpdatedAt.Format("15:04 02.01"),
 			statusStr,
-		))
+		}
+		table.Append(row)
 	}
+	table.Render()
 
 	return nil
 }
